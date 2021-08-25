d1e348a9305897a1e44e9fc0bcb96dcffc2c2b69
Ujjwal <ujjwaldash2007@gmail.com>
2021-08-25 19:32:52 +0530

    All issue resolved for python_task1

---
+++ python_task1.py
@@ -0,0 +1,49 @@
+from random import randint 
+global option
+condition=True
+c=0                          
+while condition:
+    score=20
+    turns=10
+    for i in range(1,turns+1):
+        correct_digit=0
+        Correct=[]
+        wrong_pos=0
+        num=str(randint(1000,9999))
+        print(num)
+        guess=input("Guess a 4 Digit Number:")
+        if num==guess:
+            score+=5
+            print("You Won")
+            print("Your Score is:",score)
+            c=1
+            break
+        else:
+            score-=2
+            for j in range(0,len(num)):
+                for k in range(0,len(guess)):
+                    if num[j]==guess[k]:
+                        correct_digit+=1
+                        if j==k:
+                            Correct.append(int(guess[k]))
+                        else:
+                            wrong_pos+=1
+            print("%d digits :%s guessed correctly. %d in correct position."%(correct_digit,Correct,len(Correct)))
+            if wrong_pos>0:
+                print("%d in wrong position"%(wrong_pos))
+            print("Turns remaining-",turns-i)
+    if c==1 or i==turns:
+        option=input("Do you want to play again?(y/n):").lower()
+        if option=='n':
+            condition=False
+            
+
+            
+
+        
+        
+        
+        
+    
+
+
